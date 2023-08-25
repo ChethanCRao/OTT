4152db3c2b9508878b52f2ada1c143a438005718
ChethanCRao <chethancr50@gmail.com>
2023-08-26 00:22:05 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,66 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-function App() {
-  const [banner, setBanner] = useState([]);
-  const [trending, setTrending] = useState([]);
-  const [horror, setHorror] = useState([]);
-  const [scific, setScific] = useState([]);
 
-  useEffect(() => {
-    axios.get('/banner')
-      .then(response => {
-        setBanner(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching banner content:', error);
-      });
+mongoose.connect(DB)
 
-    axios.get('/trending')
-      .then(response => {
-        setTrending(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching trending content:', error);
-      });
+app.use(cors());
 
-    axios.get('/horror')
-      .then(response => {
-        setHorror(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching horror content:', error);
-      });
+// database connection
+require("./db/conn");
+const banner = require("./model/BannerSchema");
+const horror = require("./model/horror.schema");
+const scific = require("./model/scific.schema");
+const trending = require("./model/trending.schema");
 
-    axios.get('/scific')
-      .then(response => {
-        setScific(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching scific content:', error);
-      });
-  }, []);
+app.get("/banner", (req, res) => {
+  banner.find({}).then((result) => {
+    res.send(result);
+  });
+});
 
-  return (
-    <div>
-      <h1>OTT Website</h1>
-      
-      <h2>Banner Content:</h2>
-      <ul>
-        {banner.map(item => (
-          <li key={item._id}>
-            <h3>{item.title}</h3>
-            <p>{item.description}</p>
-          </li>
-        ))}
-      </ul>
+app.get("/trending", (req, res) => {
+  trending.find({}).then((result) => {
+    res.send(result);
+  });
+});
 
-      <h2>Trending Content:</h2>
-      <ul>
-        {/* Similar rendering for trending, horror, and scific */}
-      </ul>
-    </div>
-  );
-}
+app.get("/horror", (req, res) => {
+  horror.find({}).then((result) => {
+    res.send(result);
+  });
+});
 
-export default App;
+app.get("/scific", (req, res) => {
+  scific.find({}).then((result) => {
+    res.send(result);
+  });
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server Started at ${port}`);
+});
