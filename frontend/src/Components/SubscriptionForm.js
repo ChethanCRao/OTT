da1b2a33559467bfc4193d704a752419b3d37e52
ChethanCRao <chethancr50@gmail.com>
2023-09-05 18:58:34 +0530

    commit

--- frontend/src/Components/SubscriptionForm.js
+++ frontend/src/Components/SubscriptionForm.js
@@ -1,67 +1,97 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import './SubscriptionForm.css';
-import { Link } from 'react-router-dom';
 
+export default function Subcription() {
+  const [selectedPlan, setSelectedPlan] = useState(null);
+  
 
-const SubscriptionForm = ({ onSubmit }) => {
-  const [email, setEmail] = useState('');
-  const [plan, setPlan] = useState('');
+  const plans = [
+    {
+      id: 1,
+      name: "Basic Plan",
+      price: 199,
+      features: ["480p Video Quality"], 
+      Link: "https://buy.stripe.com/test_dR6g12djf2NJ2ysbIJ",
+    },
+    {
+      id: 2,
+      name: "Premium Plan",
+      price: 299,
+      features: ["780p Video Quality"], 
+      Link: "https://buy.stripe.com/test_8wM6qsbb7fAv4GAaEG",
+    },
+    {
+      id: 3,
+      name: "Pro Plan",
+      price: 599,
+      features: ["1080p Video Quality"], 
+      Link: "https://buy.stripe.com/test_00g5mo5QNewra0UfZ1",
+    },
+  ];
 
-  const handleSubscribeClick = () => {
-    // Redirect to the specified link in a new tab/window
-    window.open('https://buy.stripe.com/test_4gweWY92Z0FB5KE288', '_blank');
+  const handlePlanSelect = (plan) => {
+    setSelectedPlan(plan);
   };
 
   return (
-    <div>
-      <b className="watchflix" style={{fontSize: "25px",marginLeft: "15px",marginTop: "10px"}}>
-      <span style={{color: "white"}}>Watch</span>
-        <span className="flix">Flix</span>
-        </b>
-        <div className="group-child" />
-        <div className="sign-up-parent">
-        <Link to="/SignUpForm" className="sign-up" style={{textDecoration: "none",color: "white"}}>
-        <b>Sign up</b>
-      </Link>
-          <img className="bx-search-icon" alt="" src="/bxbxsearch.svg" />
-          <img
-            className="claritybell-outline-badged-icon100"
-            alt=""
-            src="/claritybelloutlinebadged.svg"
-          />
-        </div>
-      <form className="custom-form" onSubmit={(e) => e.preventDefault()}>
-  <label>
-    Email:
-    <input
-      type="email"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-      required
-    />
-  </label>
-  <br />
-  <label>
-    Subscription Plan:
-    <select
-      value={plan}
-      onChange={(e) => setPlan(e.target.value)}
-      required
+    <div
+      style={{
+        background: "rgb(33, 33, 33)",
+        color: "white",
+        textAlign: "center",
+        marginTop: "100px",
+      }}
     >
-      <option value="">Select a plan</option>
-      <option value="basic">Basic 199/-</option>
-      <option value="premium">Premium 299/-</option>
-      <option value="pro">Pro 599/-</option>
-    </select>
-  </label>
-  <br />
-  <button type="button" onClick={handleSubscribeClick}>
-    Subscribe
-  </button>
-</form>
+      <h1>Choose a Plan</h1>
+      <div
+        className="plans" 
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        {plans.map((plan) => (
+          <div
+            key={plan.id}
+            className={`plan {selectedPlan === plan ? "selected" : ""}`}
+            onClick={() => handlePlanSelect(plan)}
+            style={{
+              border: "3px solid orangered",
+              margin: "30px",
+              padding: "20px",
+              borderRadius: "30px",
+              cursor: "pointer",
+            }}
+          >
+            <h2>{plan.name}</h2>
+            <p>Price: {plan.price} / Month</p>
+            <p>{plan.features}</p>
+          </div>
+        ))}
+      </div>
+      <div className="selected-plan">
+        {selectedPlan ? (
+          <div>
+            <h3>Selected Plan: {selectedPlan.name}</h3>
+            <p>Price: {selectedPlan.price} / Month</p>
+          </div>
+        ) : (
+          <p style={{fontFamily: "times new roman",fontWeight: "bold"}}>No Plan Selected.</p>
+        )}
+      </div>
+      <button
+  disabled={!selectedPlan}
+  style={{
+    margin: "10px",
+    fontWeight: "bold", // Add this line to make the text bold
+  }}
+  onClick={() => {
+    window.open(`${selectedPlan.Link}`, "_blank");
+  }}
+>
+  Pay Now
+</button>
 
     </div>
   );
-};
-
-export default SubscriptionForm;
+}
