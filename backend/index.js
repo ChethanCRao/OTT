4169f0f924b3be0b05bc5ca5180aa3f4e3570149
Chethan C Rao <chethancr50@gmail.com>
2023-09-21 03:00:59 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -6,45 +6,38 @@ const cors = require("cors");
 
 app.use(cors());
 
-// Importing your database connection from "./db/conn"
 require("./db/conn");
 
-// Importing your models
 const banner = require("./model/BannerSchema");
 const horror = require("./model/horror.schema");
 const scific = require("./model/scific.schema");
 const trending = require("./model/trending.schema");
 
 app.get("/banner", (req, res) => {
-  // You should query the "banner" model, not "banner.find({})"
   banner.find({}).then((result) => {
     res.send(result);
   });
 });
 
 app.get("/", (req, res) => {
-  // Assuming you meant to query the "trending" model
   trending.find({}).then((result) => {
     res.send(result);
   });
 });
 
 app.get("/trending", (req, res) => {
-  // Assuming you meant to query the "trending" model
   trending.find({}).then((result) => {
     res.send(result);
   });
 });
 
 app.get("/horror", (req, res) => {
-  // Assuming you meant to query the "horror" model
   horror.find({}).then((result) => {
     res.send(result);
   });
 });
 
 app.get("/scific", (req, res) => {
-  // Assuming you meant to query the "scific" model
   scific.find({}).then((result) => {
     res.send(result);
   });
